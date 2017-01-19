ba80c8e70c44944b6dab3c1d4169501626980d19
knyuni <ndugirekanyoni@gmail.com>
2017-01-19 12:46:30 +0300

    Missing Numbers Trial

---
+++ Lab_3/MissingNo/MissingNo1.py
@@ -0,0 +1,12 @@
+a = [4,66,7]
+b =  [66,77,7,4]
+aList = []
+if len(a) and len(b) > 0:
+  if a.count == b.count:
+    c=0
+  else:
+    c= list(set(a).symmetric_difference(b))
+ # c= set(a).symmetric_difference(b)
+else:
+  c=0
+print (c)
