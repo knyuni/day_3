895e37cd71acb454cdeec61bbef1aff1fe645502
knyuni <ndugirekanyoni@gmail.com>
2017-01-19 17:59:53 +0300

    Binary Search(TotalHelpRequired)

---
+++ Lab_3/Binary_Search/BinarySearch.py
@@ -0,0 +1,31 @@
+class binary_search(object):
+	def __init__(self, data=None, left=None, right=None):
+		self.left = left
+        self.right = right
+        self.data = data
+
+    def getLeftChild(self):
+        return self.left
+    def getRightChild(self):
+        return self.right
+    def setNodeValue(self,value):
+        self.rootid = value
+    def getNodeValue(self):
+        return self.rootid
+		
+
+
+	def search(a, b):
+		a = 0
+	    b = len(a)-1
+	    length = False
+	    if node is None:
+	    	return False
+   		else:
+       		if node.data == item:
+           		return True
+       	elif node.data < item:
+           	return self.search(node.right, item)
+       	else:
+           	return self.search(node.left, item)
+	
